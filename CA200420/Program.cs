c3a13ae6bebcb2e88798ef03e8b6106d3acc685d
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-04-20 10:11:24 +0200

    Add project files.

---
+++ CA200420/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200420
+{
+    class Program
+    {
+        static void Main()
+        {
+            var pc = new Szamitogep();
+
+            //pc.Processor = null; ===> nem jó!
+            //pc.Processor = "Texas Holdem TST2";  ===> nem jó!
+
+            pc.Processor = "krumpli"; //===>jó!
+            Console.WriteLine($"A PC-nek a processora: {pc.Processor}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CA200420/Szamitogep.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200420
+{
+    class Szamitogep
+    {
+        private static readonly List<string> _processorLista = new List<string>()
+        {
+            "Intel Core i5",
+            "Intel Core i7",
+            "Intel Core i9",
+            "AMD Ryzen 5",
+            "AMD Ryzen 7",
+        };
+
+
+        private string _processor;
+        public string Processor
+        {
+            set
+            {
+                if (value is null)
+                {
+                    throw new Exception("a számítógép nem működik processzor nélkül!");
+                }
+
+                if (!_processorLista.Contains(value))
+                {
+                    throw new Exception("ilyen processor nincs az adatbázisban!");
+                }
+
+                _processor = value;
+            }
+            get { return _processor; }
+        }
+
+
+        //gpu
+            //GTX 2060
+            //AMD RX570 
+        //ram
+            //4 gb
+            //8 gb
+            //16 gb
+        //drive
+            //tipus - hdd / ssd
+            //500Gb, 1T, 2T - 128 - 256 - 512           
+        //Motherbord (+cs)
+            //Z390
+            //A320
+            //X570
+    }
+}
